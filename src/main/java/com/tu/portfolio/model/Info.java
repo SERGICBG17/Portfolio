54526bec8a141398dc8c64c2a4ad82900377fd07
SERGICBG17 <sergiocasin3@gmail.com>
2026-01-12 11:57:29 +0100

    2.0: Clase Info rellenada

--- src/main/java/com/tu/portfolio/model/Info.java
+++ src/main/java/com/tu/portfolio/model/Info.java
@@ -1,4 +1,104 @@
 package com.tu.portfolio.model;
 
+import jakarta.persistence.*;
+import org.springframework.data.annotation.Id;
+
+@Entity
+@Table(name = "info")
 public class Info {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+
+    String nombre;//no nulo
+    String descripcion;
+    String foto_url;
+    String email;//no nulo
+    String telefono;
+    String linkedin;
+    String github;
+
+    public Info() {}
+
+    public Info(long id, String nombre,String email) {
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+    }
+
+    public Info(long id, String nombre, String descripcion, String foto_url, String email, String telefono, String linkedin, String github) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.foto_url = foto_url;
+        this.email = email;
+        this.telefono = telefono;
+        this.linkedin = linkedin;
+        this.github = github;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getFoto_url() {
+        return foto_url;
+    }
+
+    public void setFoto_url(String foto_url) {
+        this.foto_url = foto_url;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getLinkedin() {
+        return linkedin;
+    }
+
+    public void setLinkedin(String linkedin) {
+        this.linkedin = linkedin;
+    }
+
+    public String getGithub() {
+        return github;
+    }
+
+    public void setGithub(String github) {
+        this.github = github;
+    }
 }
