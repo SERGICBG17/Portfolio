5f16c781774ab2c324897dc6333d1963262da644
SERGICBG17 <sergiocasin3@gmail.com>
2026-01-12 11:57:50 +0100

    2.1: Clase Experiencia rellenada

--- src/main/java/com/tu/portfolio/model/Experiencia.java
+++ src/main/java/com/tu/portfolio/model/Experiencia.java
@@ -1,4 +1,89 @@
 package com.tu.portfolio.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import org.springframework.data.annotation.Id;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "experiencia")
 public class Experiencia {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+
+    String puesto;//no nulo
+    String empresa;//no nulo
+    String descripcion;
+    Date fecha_inicio;
+    Date fecha_fin;
+
+    public Experiencia() {}
+
+    public Experiencia(long id, String puesto, String empresa) {
+        this.id = id;
+        this.puesto = puesto;
+        this.empresa = empresa;
+    }
+
+    public Experiencia(long id, String puesto, String empresa, String descripcion, Date fecha_inicio, Date fecha_fin) {
+        this.id = id;
+        this.puesto = puesto;
+        this.empresa = empresa;
+        this.descripcion = descripcion;
+        this.fecha_inicio = fecha_inicio;
+        this.fecha_fin = fecha_fin;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getPuesto() {
+        return puesto;
+    }
+
+    public void setPuesto(String puesto) {
+        this.puesto = puesto;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Date getFecha_inicio() {
+        return fecha_inicio;
+    }
+
+    public void setFecha_inicio(Date fecha_inicio) {
+        this.fecha_inicio = fecha_inicio;
+    }
+
+    public Date getFecha_fin() {
+        return fecha_fin;
+    }
+
+    public void setFecha_fin(Date fecha_fin) {
+        this.fecha_fin = fecha_fin;
+    }
 }
