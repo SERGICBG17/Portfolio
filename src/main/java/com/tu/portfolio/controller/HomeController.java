9e5e506481e0dd2bd07cacced9bf00e55370e63d
SERGICBG17 <sergiocasin3@gmail.com>
2026-01-14 13:38:08 +0100

    4.0: Modificado el HomeController, para que muestre el índice de la página, todavía la página no tiene nada simplemente están escritos un mensaje de prueba de hola

--- src/main/java/com/tu/portfolio/controller/HomeController.java
+++ src/main/java/com/tu/portfolio/controller/HomeController.java
@@ -1,4 +1,13 @@
 package com.tu.portfolio.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
+@Controller
 public class HomeController {
+
+    @RequestMapping("/")
+    String index() {
+        return "index";
+    }
+
 }
